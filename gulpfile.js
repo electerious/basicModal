8f260e2ec9b8de01688fba17523cd7ff2ae81092
Tobias Reich <tobias.reich.ich@gmail.com>
2014-11-26 12:26:18 +0100

    Changed folder-structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,9 @@
 var	gulp = require('gulp'),
 	plugins = require('gulp-load-plugins')();
 
-gulp.task('sass', function () {
+gulp.task('styles', function () {
 
-	gulp.src('./src/scss/main.scss')
+	gulp.src('./src/styles/main.scss')
 		.pipe(plugins.sass())
 		.pipe(plugins.concat('basicModal.min.css', {newLine: "\n"}))
 		.pipe(plugins.autoprefixer('last 2 version', '> 1%'))
@@ -12,9 +12,9 @@ gulp.task('sass', function () {
 
 });
 
-gulp.task('coffee', function () {
+gulp.task('scripts', function () {
 
-	gulp.src('./src/coffee/*.coffee')
+	gulp.src('./src/scripts/*.coffee')
 		.pipe(plugins.coffee({bare: true}).on('error', plugins.util.log))
 		.pipe(plugins.concat('basicModal.min.js', {newLine: "\n"}))
 		.pipe(plugins.uglify())
@@ -22,4 +22,4 @@ gulp.task('coffee', function () {
 
 });
 
-gulp.task('default', ['sass', 'coffee']);
+gulp.task('default', ['styles', 'scripts']);
