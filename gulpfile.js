63d48751af043948953a295ed35bb511830d5e78
Tobias Reich <tobias.reich.ich@gmail.com>
2015-06-14 18:10:46 +0200

    Support for CommonJS and AMD

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,47 @@
-var	gulp = require('gulp'),
-	plugins = require('gulp-load-plugins')();
+var	name    = require('./package.json').moduleName,
+    fs      = require('fs'),
+    gulp    = require('gulp'),
+    plugins = require('gulp-load-plugins')()
 
-gulp.task('styles', function () {
+var head = fs.readFileSync('./node_modules/@electerious/modulizer/head.js', { encoding: 'utf8' }),
+    foot = fs.readFileSync('./node_modules/@electerious/modulizer/foot.js', { encoding: 'utf8' })
+
+var catchError = function(err) {
+
+	console.log(err.toString())
+	this.emit('end')
+
+}
+
+gulp.task('styles', function() {
 
 	gulp.src('./src/styles/main.scss')
-		.pipe(plugins.sass())
-		.pipe(plugins.concat('basicModal.min.css', {newLine: "\n"}))
-		.pipe(plugins.autoprefixer('last 2 version', '> 1%'))
-		.pipe(plugins.minifyCss())
-		.pipe(gulp.dest('./dist'));
+	    .pipe(plugins.sass())
+	    .pipe(plugins.concat(name + '.min.css', { newLine: "\n" }))
+	    .pipe(plugins.autoprefixer('last 2 version', '> 1%'))
+	    .pipe(plugins.minifyCss())
+	    .pipe(gulp.dest('./dist'))
 
-});
+})
 
-gulp.task('scripts', function () {
+gulp.task('scripts', function() {
 
 	gulp.src('./src/scripts/*.js')
-		.pipe(plugins.babel())
-		.pipe(plugins.concat('basicModal.min.js', {newLine: "\n"}))
-		.pipe(plugins.uglify())
-		.pipe(gulp.dest('./dist'));
+	    .pipe(plugins.header(head, { name: name }))
+	    .pipe(plugins.footer(foot))
+	    .pipe(plugins.babel())
+	    .pipe(plugins.concat(name + '.min.js', { newLine: "\n" }))
+	    // .pipe(plugins.uglify())
+	    .on('error', catchError)
+	    .pipe(gulp.dest('./dist'))
 
-});
+})
 
-gulp.task('default', ['styles', 'scripts']);
+gulp.task('default', ['styles', 'scripts'])
 
-gulp.task('watch', function() {
-	gulp.watch('./src/styles/*.scss', ['styles']);
-	gulp.watch('./src/scripts/*.js', ['scripts']);
-});
+gulp.task('watch', ['styles', 'scripts'], function() {
+
+	gulp.watch('./src/styles/*.scss', ['styles'])
+	gulp.watch('./src/scripts/*.js', ['scripts'])
+
+})
--- src/scripts/basicModal.js
+++ src/scripts/basicModal.js
@@ -1,306 +1,314 @@
-window.basicModal = {
+var lastFocus = null
 
-	THEME: {
-		small  : 'basicModal__small',
-		xclose : 'basicModal__xclose'
-	},
+const THEME = {
+	small  : 'basicModal__small',
+	xclose : 'basicModal__xclose'
+}
 
-	_lastFocus: null,
+const dom = function(elem = '', multiple = false) {
 
-	_dom(elem = '', multiple = false) {
+	if (multiple===true) return document.querySelectorAll('.basicModal ' + elem)
+	else                 return document.querySelector('.basicModal ' + elem)
 
-		if (multiple===true) return document.querySelectorAll('.basicModal ' + elem)
-		else                 return document.querySelector('.basicModal ' + elem)
+}
 
-	},
+const valid = function(data = {}) {
 
-	_valid(data = {}) {
+	if (data.body==null)       data.body = ''
+	if (data.class==null)      data.class = ''
+	if (data.closable!==false) data.closable = true
 
-		if (data.body==null)       data.body = ''
-		if (data.class==null)      data.class = ''
-		if (data.closable!==false) data.closable = true
+	if (data.buttons==null) {
+		console.error('basicModal requires at least one button')
+		return false
+	}
 
-		if (data.buttons==null) {
-			console.error('basicModal requires at least one button')
+	// Validate action-button
+	if (data.buttons.action!=null) {
+
+		if (data.buttons.action.class==null) data.buttons.action.class = ''
+		if (data.buttons.action.title==null) data.buttons.action.title = 'OK'
+
+		if (data.buttons.action.fn==null) {
+			console.error('Missing fn for action-button')
 			return false
 		}
 
-		// Validate action-button
-		if (data.buttons.action!=null) {
+	}
 
-			if (data.buttons.action.class==null) data.buttons.action.class = ''
-			if (data.buttons.action.title==null) data.buttons.action.title = 'OK'
+	// Validate cancel-button
+	if (data.buttons.cancel!=null) {
 
-			if (data.buttons.action.fn==null) {
-				console.error('Missing fn for action-button')
-				return false
-			}
+		if (data.buttons.cancel.class==null) data.buttons.cancel.class = ''
+		if (data.buttons.cancel.title==null) data.buttons.cancel.title = 'Cancel'
 
-		}
-
-		// Validate cancel-button
-		if (data.buttons.cancel!=null) {
-
-			if (data.buttons.cancel.class==null) data.buttons.cancel.class = ''
-			if (data.buttons.cancel.title==null) data.buttons.cancel.title = 'Cancel'
-
-			if (data.buttons.cancel.fn==null) {
-				console.error('Missing fn for cancel-button')
-				return false
-			}
-
-		}
-
-		return true
-
-	},
-
-	_build(data) {
-
-		var icon = '<svg xmlns="http://www.w3.org/2000/svg" width="512" height="512" viewBox="0 0 512 512"><path d="M405 136.798l-29.798-29.798-119.202 119.202-119.202-119.202-29.798 29.798 119.202 119.202-119.202 119.202 29.798 29.798 119.202-119.202 119.202 119.202 29.798-29.798-119.202-119.202z"/></svg>',
-		    html = ''
-
-		html += `
-		        <div class='basicModalContainer basicModalContainer--fadeIn' data-closable='${ data.closable }'>
-		            <div class='basicModal basicModal--fadeIn ${ data.class }' role="dialog">
-		                <div class='basicModal__content'>
-		                    ${ data.body }
-		                </div>
-		                <div class='basicModal__buttons'>
-		        `
-
-		// Cancel-button
-		if (data.buttons.cancel!=null) {
-			if (data.buttons.cancel.class.indexOf('basicModal__xclose')===-1) {
-
-				// Default close-button
-				html +=	`<a id='basicModal__cancel' class='basicModal__button ${ data.buttons.cancel.class }'>${ data.buttons.cancel.title }</a>`
-
-			} else {
-
-				// Custom close-button for the login-theme
-				html += `<div id='basicModal__cancel' class='basicModal__button ${ data.buttons.cancel.class }' aria-label='close'>${ icon }</div>`
-
-			}
-		}
-
-		// Action-button
-		if (data.buttons.action!=null) {
-
-			html += `<a id='basicModal__action' class='basicModal__button ${ data.buttons.action.class }'>${ data.buttons.action.title }</a>`
-
-		}
-
-		html += `
-		                </div>
-		            </div>
-		        </div>
-		        `
-
-		return html
-
-	},
-
-	getValues() {
-
-		var values = null,
-		    inputs = basicModal._dom('input', true)
-
-		if (inputs.length>0) {
-
-			values = {}
-
-			// Get value from all inputs
-			for (let i = 0; i < inputs.length; ++i) {
-
-				let input = inputs[i],
-				    name  = input.getAttribute('name'),
-				    value = input.value
-
-				// Store name and value of input
-				if (name!=null) values[name] = value
-
-			}
-
-			// Set value back to null when object empty
-			if (Object.keys(values).length===0) values = null
-
-		}
-
-		return values
-
-	},
-
-	_bind(data) {
-
-		// Cancel-button
-		if (data.buttons.cancel!=null) {
-
-			basicModal._dom('#basicModal__cancel').onclick = function() {
-
-				// Don't execute function when button has been clicked already
-				if (this.classList.contains('basicModal__button--active')===true) return false
-
-				this.classList.add('basicModal__button--active')
-				data.buttons.cancel.fn()
-
-			}
-
-		}
-
-		// Action-button
-		if (data.buttons.action!=null) {
-
-			basicModal._dom('#basicModal__action').onclick = function() {
-
-				// Don't execute function when button has been clicked already
-				if (this.classList.contains('basicModal__button--active')===true) return false
-
-				this.classList.add('basicModal__button--active')
-				data.buttons.action.fn(basicModal.getValues())
-
-			}
-
-		}
-
-		// Bind input
-		var inputs = basicModal._dom('input', true)
-		for (let i = 0; i < inputs.length; ++i) { inputs[i].onkeydown = function() { this.classList.remove('error') } }
-
-		return true
-
-	},
-
-	show(data) {
-
-		if (data==null || Object.keys(data).length===0) return false
-
-		// Save focused element
-		basicModal._lastFocus = document.activeElement
-
-		// Validate data and set default values
-		if (basicModal._valid(data)===false) return false
-
-		// Close open modal
-		if (basicModal._dom()!=null) {
-			basicModal.close(true)
-			setTimeout(() => basicModal.show(data), 301)
+		if (data.buttons.cancel.fn==null) {
+			console.error('Missing fn for cancel-button')
 			return false
 		}
 
-		// Build and append modal to DOM
-		var html = basicModal._build(data)
-		document.body.insertAdjacentHTML('beforeend', html)
+	}
 
-		// Bind elements
-		basicModal._bind(data)
+	return true
 
-		// Select the first input when available
-		var input = basicModal._dom('input')
-		if (input!=null) input.select()
+}
 
-		// Execute callback when available
-		if (data.callback!=null) data.callback(data)
+const build = function(data) {
 
-		return true
+	var icon = '<svg xmlns="http://www.w3.org/2000/svg" width="512" height="512" viewBox="0 0 512 512"><path d="M405 136.798l-29.798-29.798-119.202 119.202-119.202-119.202-29.798 29.798 119.202 119.202-119.202 119.202 29.798 29.798 119.202-119.202 119.202 119.202 29.798-29.798-119.202-119.202z"/></svg>',
+	    html = ''
 
-	},
+	html += `
+	        <div class='basicModalContainer basicModalContainer--fadeIn' data-closable='${ data.closable }'>
+	            <div class='basicModal basicModal--fadeIn ${ data.class }' role="dialog">
+	                <div class='basicModal__content'>
+	                    ${ data.body }
+	                </div>
+	                <div class='basicModal__buttons'>
+	        `
 
-	error(input) {
+	// Cancel-button
+	if (data.buttons.cancel!=null) {
+		if (data.buttons.cancel.class.indexOf('basicModal__xclose')===-1) {
 
-		// Reactive buttons and remove old errors
-		basicModal.reset()
+			// Default close-button
+			html +=	`<a id='basicModal__cancel' class='basicModal__button ${ data.buttons.cancel.class }'>${ data.buttons.cancel.title }</a>`
 
-		var elem = basicModal._dom(`input[name='${ input }']`)
+		} else {
 
-		elem.classList.add('error')
-		elem.select()
+			// Custom close-button for the login-theme
+			html += `<div id='basicModal__cancel' class='basicModal__button ${ data.buttons.cancel.class }' aria-label='close'>${ icon }</div>`
 
-		// Shake input
-		basicModal._dom().classList.remove('basicModal--fadeIn', 'basicModal--shake')
-		setTimeout(() => basicModal._dom().classList.add('basicModal--shake'), 1)
+		}
+	}
 
-	},
+	// Action-button
+	if (data.buttons.action!=null) {
 
-	visible() {
+		html += `<a id='basicModal__action' class='basicModal__button ${ data.buttons.action.class }'>${ data.buttons.action.title }</a>`
 
-		if (basicModal._dom()!=null) return true
-		return false
+	}
 
-	},
+	html += `
+	                </div>
+	            </div>
+	        </div>
+	        `
 
-	action() {
+	return html
 
-		var elem = basicModal._dom('#basicModal__action')
+}
 
-		if (elem!=null) {
+const getValues = function() {
 
-			elem.click()
-			return true
+	var values = null,
+	    inputs = dom('input', true)
+
+	if (inputs.length>0) {
+
+		values = {}
+
+		// Get value from all inputs
+		for (let i = 0; i < inputs.length; ++i) {
+
+			let input = inputs[i],
+			    name  = input.getAttribute('name'),
+			    value = input.value
+
+			// Store name and value of input
+			if (name!=null) values[name] = value
 
 		}
 
-		return false
+		// Set value back to null when object empty
+		if (Object.keys(values).length===0) values = null
 
-	},
+	}
 
-	cancel() {
+	return values
 
-		var elem = basicModal._dom('#basicModal__cancel')
+}
 
-		if (elem!=null) {
+const bind = function(data) {
 
-			elem.click()
-			return true
+	// Cancel-button
+	if (data.buttons.cancel!=null) {
+
+		dom('#basicModal__cancel').onclick = function() {
+
+			// Don't execute function when button has been clicked already
+			if (this.classList.contains('basicModal__button--active')===true) return false
+
+			this.classList.add('basicModal__button--active')
+			data.buttons.cancel.fn()
 
 		}
 
-		return false
+	}
 
-	},
+	// Action-button
+	if (data.buttons.action!=null) {
 
-	reset() {
+		dom('#basicModal__action').onclick = function() {
 
-		// Reactive buttons
-		var buttons = basicModal._dom('.basicModal__button', true)
-		for (let i = 0; i < buttons.length; ++i) { buttons[i].classList.remove('basicModal__button--active') }
+			// Don't execute function when button has been clicked already
+			if (this.classList.contains('basicModal__button--active')===true) return false
 
-		// Remove errors
-		var inputs = basicModal._dom('input', true)
-		for (let i = 0; i < inputs.length; ++i) { inputs[i].classList.remove('error') }
+			this.classList.add('basicModal__button--active')
+			data.buttons.action.fn(getValues())
 
-		return true
-
-	},
-
-	close(force) {
-
-		// Only close when a modal is visible
-		if (basicModal.visible()===false) return false
-
-		// Validate force
-		if (force!==true) force = false
-
-		// Get modal container
-		var container = basicModal._dom().parentElement
-
-		// Don't close when modal not closable
-		// Use 'force===true' to close unclosebale modal
-		if (container.getAttribute('data-closable')==='false' && force===false) return false
-
-		container.classList.remove('basicModalContainer--fadeIn')
-		container.classList.add('basicModalContainer--fadeOut')
-
-		setTimeout(() => container.parentNode.removeChild(container), 300)
-
-		// Restore last active element
-		if (basicModal._lastFocus!=null) {
-			basicModal._lastFocus.focus()
-			basicModal._lastFocus = null
 		}
 
+	}
+
+	// Bind input
+	var inputs = dom('input', true)
+	for (let i = 0; i < inputs.length; ++i) { inputs[i].onkeydown = function() { this.classList.remove('error') } }
+
+	return true
+
+}
+
+const show = function(data) {
+
+	if (data==null || Object.keys(data).length===0) return false
+
+	// Save focused element
+	lastFocus = document.activeElement
+
+	// Validate data and set default values
+	if (valid(data)===false) return false
+
+	// Close open modal
+	if (dom()!=null) {
+		close(true)
+		setTimeout(() => show(data), 301)
+		return false
+	}
+
+	// Build and append modal to DOM
+	var html = build(data)
+	document.body.insertAdjacentHTML('beforeend', html)
+
+	// Bind elements
+	bind(data)
+
+	// Select the first input when available
+	var input = dom('input')
+	if (input!=null) input.select()
+
+	// Execute callback when available
+	if (data.callback!=null) data.callback(data)
+
+	return true
+
+}
+
+const error = function(input) {
+
+	// Reactive buttons and remove old errors
+	reset()
+
+	var elem = dom(`input[name='${ input }']`)
+
+	elem.classList.add('error')
+	elem.select()
+
+	// Shake input
+	dom().classList.remove('basicModal--fadeIn', 'basicModal--shake')
+	setTimeout(() => dom().classList.add('basicModal--shake'), 1)
+
+}
+
+const visible = function() {
+
+	if (dom()!=null) return true
+	return false
+
+}
+
+const action = function() {
+
+	var elem = dom('#basicModal__action')
+
+	if (elem!=null) {
+
+		elem.click()
 		return true
 
 	}
 
+	return false
+
+}
+
+const cancel = function() {
+
+	var elem = dom('#basicModal__cancel')
+
+	if (elem!=null) {
+
+		elem.click()
+		return true
+
+	}
+
+	return false
+
+}
+
+const reset = function() {
+
+	// Reactive buttons
+	var buttons = dom('.basicModal__button', true)
+	for (let i = 0; i < buttons.length; ++i) { buttons[i].classList.remove('basicModal__button--active') }
+
+	// Remove errors
+	var inputs = dom('input', true)
+	for (let i = 0; i < inputs.length; ++i) { inputs[i].classList.remove('error') }
+
+	return true
+
+}
+
+const close = function(force) {
+
+	// Only close when a modal is visible
+	if (visible()===false) return false
+
+	// Validate force
+	if (force!==true) force = false
+
+	// Get modal container
+	var container = dom().parentElement
+
+	// Don't close when modal not closable
+	// Use 'force===true' to close unclosebale modal
+	if (container.getAttribute('data-closable')==='false' && force===false) return false
+
+	container.classList.remove('basicModalContainer--fadeIn')
+	container.classList.add('basicModalContainer--fadeOut')
+
+	setTimeout(() => container.parentNode.removeChild(container), 300)
+
+	// Restore last active element
+	if (lastFocus!=null) {
+		lastFocus.focus()
+		lastFocus = null
+	}
+
+	return true
+
+}
+
+return {
+	THEME,
+	show,
+	visible,
+	getValues,
+	action,
+	cancel,
+	error,
+	reset,
+	close
 }
